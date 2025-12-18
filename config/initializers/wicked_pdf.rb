0699ef58ef297bc662c628c836268b5a7de5427e
MARTIN ESQUERCIA <martincitoesquercia@hotmail.com>
2025-12-18 12:03:35 -0300

    Agrego compatibilidad para linux y mac

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,21 +1,14 @@
-# WickedPDF Global Configuration
-#
-# Use this to set up shared configuration options for your entire application.
-# Any of the configuration options shown here can also be applied to single
-# models by passing arguments to the `render :pdf` call.
-#
-# To learn more, check out the README:
-#
-# https://github.com/mileszs/wicked_pdf/blob/master/README.md
-
 WickedPdf.configure do |config|
-  # Ruta al ejecutable de wkhtmltopdf en Windows
-  config.exe_path = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+  if Gem.win_platform?
+    # Windows
+    config.exe_path = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+  else
+    # Linux / macOS
+    # wkhtmltopdf debe estar instalado y disponible en el PATH
+    config.exe_path = `which wkhtmltopdf`.strip
+  end
 
-  # Necesario para que wkhtmltopdf pueda leer assets locales (CSS, imágenes)
   config.enable_local_file_access = true
-
-  # Layout por defecto (opcional, pero recomendado)
   config.layout = 'pdf'
 end
 
